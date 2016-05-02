01033c108b357dcb477adf22387231dd69da6386
codecademy <ccuser@codecademy.com>
2016-05-02 14:54:29 -0400

    Slider files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,3 @@
-Rails.application.config.assets.precompile += %w( jumbotron-narrow.css)
-Rails.application.config.assets.precompile += %w( unslider-min.js )
+Rails.application.config.assets.precompile += %w(jumbotron-narrow.css)
+Rails.application.config.assets.precompile += %w(unslider-min.js)
+Rails.application.config.assets.precompile += %w(unslider-dots.css)
